c9246f31f99487241393a65533cc82fa2f5064c7
chifeizhai <465522282@qq.com>
2020-09-09 17:29:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Header/>
+    <div class="main">
+      <router-view name="slidebar"></router-view>
+      <router-view name="main"></router-view>
+    </div>
+  </div>     
+</template>
+
+<script>
+import Header from './components/Header'
+import Postlist from './components/Postlist'
+export default {
+  name: 'App',
+  components:{
+    Header,
+    Postlist
+  }
+}
+</script>
+
+<style>
+.main{
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
